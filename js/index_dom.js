867e90cf595fdc78da781501994496cdaa8bdfbe
federicovarelamore <85002323+federicovarelamore@users.noreply.github.com>
2022-04-17 00:09:34 -0300

    Finished hamburger menu

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,1 +1,7 @@
+import hamburgerMenu from "./menu_hamburguesa.js";
 
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  hamburgerMenu(".panel-btn", ".panel", ".menu a");
+});
